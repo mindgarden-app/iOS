7129808c97ea8022d7af4a2f7cf6157d48091399
Sunghee <630sunghee@gmail.com>
2019-07-02 01:27:12 +0900

    Add a change in year to main pop up

--- MindGarden/Sources/ViewControllers/Main/PopUpVC.swift
+++ MindGarden/Sources/ViewControllers/Main/PopUpVC.swift
@@ -15,21 +15,45 @@ protocol sendDataToViewProtocol {
 class PopUpVC: UIViewController {
     
     var delegate:sendDataToViewProtocol? = nil
+    private var year: Int = 0;
 
     @IBOutlet var popUpView: UIView!
     @IBOutlet var yearLabel: UILabel!
+    @IBOutlet var leftBtn: UIButton!
+    @IBOutlet var rightBtn: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setYearLabel(year: year)
 
         self.view.backgroundColor = UIColor.black.withAlphaComponent(0.3)
         popUpView.setBorder(borderColor: UIColor.gray, borderWidth: 1.0)
         popUpView.makeRounded(cornerRadius: 15)
         popUpView.dropShadow(color: UIColor.gray, offSet: CGSize(width: 0.0, height: 3.0), opacity: 0.52, radius: 3)
-//        self.view.setBorder(borderColor: UIColor.gray, borderWidth: 1.0)
-//        self.view.makeRounded(cornerRadius: nil)
     }
     
+    func setYearLabel(year: Int) {
+        if year != 0 {
+            yearLabel.text = String(year)
+        } else {
+            let calendar = Calendar.current
+            let defaultYear = calendar.component(.year, from: Date())
+            self.year = defaultYear
+            yearLabel.text = String(defaultYear)
+        }
+    }
+    
+    @IBAction func leftBtnAction(_ sender: Any) {
+        year -= 1
+        setYearLabel(year: year)
+    }
+    
+    
+    @IBAction func rightBtnAction(_ sender: Any) {
+        year += 1
+        setYearLabel(year: year)
+    }
     
     @IBAction func numBtnAction(_ sender: Any) {
         if(delegate != nil){
