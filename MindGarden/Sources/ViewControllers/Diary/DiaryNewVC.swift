94d28d029b66f60c177af1a1a53d841a94308548
Sunghee <630sunghee@gmail.com>
2019-07-05 20:22:50 +0900

    Get image from photo library

--- MindGarden/Sources/ViewControllers/Diary/DiaryNewVC.swift
+++ MindGarden/Sources/ViewControllers/Diary/DiaryNewVC.swift
@@ -14,17 +14,24 @@ class DiaryNewVC: UIViewController {
     @IBOutlet var moodTextBtn: UIButton!
     @IBOutlet var moodImgBtn: UIButton!
     @IBOutlet var inputTextView: UITextView!
+    @IBOutlet var inputImageView: UIImageView!
+    
+    let picker = UIImagePickerController()
+    var placeholder = "내용"
+    var moodText: String = ""
+    var moodImg: String = ""
+    var body: String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         setNavigationBar()
         
+        picker.delegate = self
+        
         setTextView()
         inputTextView.delegate = self
         
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
     
     func setNavigationBar() {
@@ -38,12 +45,19 @@ class DiaryNewVC: UIViewController {
         let dayOfTheWeekStr: String? = today.getDayOfTheWeek()
         
         self.navigationItem.title = "\(dateStr) (\(String(dayOfTheWeekStr!)))"
+        
+//        let rightBtn = UIBarButtonItem(image: UIImage(named: "btnRegister"), style: .plain, target: self, action: Selector(""))
+//        self.navigationItem.rightBarButtonItem = rightBtn
     }
     
     func setTextView() {
-        inputTextView.text = "내용"
+        inputTextView.text = placeholder
         inputTextView.textColor = UIColor.lightGray
     }
+    
+//    func setImageView() {
+//        let imageView = UIImageView(image: image!)
+//    }
 
     @IBAction func backBtnAction(_ sender: Any) {
         self.navigationController?.popViewController(animated: true)
@@ -57,7 +71,13 @@ class DiaryNewVC: UIViewController {
         self.view.addSubview(popUpVC.view)
         popUpVC.didMove(toParent: self)
     }
-
+    
+    
+    @IBAction func galleryBtnAction(_ sender: Any) {
+        picker.sourceType = .photoLibrary
+        present(picker, animated: true, completion: nil)
+    }
+    
     @objc func keyboardWillShow(notification: Notification) {
         if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
             print("notification: Keyboard will show")
@@ -73,21 +93,20 @@ class DiaryNewVC: UIViewController {
                 self.view.frame.origin.y += keyboardSize.height
             }
         }
-
     }
 }
 
 extension DiaryNewVC: UITextViewDelegate {
-    func textViewDidBeginEditing(textView: UITextView) {
-        if textView.textColor == UIColor.lightGray {
-            textView.text = nil
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if textView.text == placeholder {
+            textView.text = ""
             textView.textColor = UIColor.black
         }
     }
     
-    func textViewDidEndEditing(textView: UITextView) {
-        if textView.text.isEmpty {
-            textView.text = "내용"
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if textView.text == "" {
+            textView.text = placeholder
             textView.textColor = UIColor.lightGray
         }
     }
@@ -100,6 +119,23 @@ extension DiaryNewVC: MoodDelegate {
         print(text)
         moodImgBtn.setImage(UIImage(named: img), for: .normal)
         moodTextBtn.setTitle(text, for: .normal)
+        moodTextBtn.setTitleColor(UIColor.Gray, for: .normal)
     }
     
+}
+
+extension DiaryNewVC : UIImagePickerControllerDelegate,
+UINavigationControllerDelegate{
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let pickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else {
+            return
+        }
+
+//        inputImageView.frame.size = CGSize(width: 100, height: 100)
+        inputImageView.image = pickedImage
+        inputImageView.frame = CGRect(x: 0, y: 0, width: 50, height: UIScreen.main.bounds.height * 0.2)
+
+        self.dismiss(animated: true, completion: nil)
+    }
 }
