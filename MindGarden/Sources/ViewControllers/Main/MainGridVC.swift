10c8edc28db021838c32b3fd4e28da1ed908a8ff
Sunghee <630sunghee@gmail.com>
2019-07-12 00:40:35 +0900

    Fix a bug in main grid
    셀 선택시 다른 셀의 배경색이 변경되는 버그 해결

--- MindGarden/Sources/ViewControllers/Main/MainGridVC.swift
+++ MindGarden/Sources/ViewControllers/Main/MainGridVC.swift
@@ -21,11 +21,17 @@ class MainGridVC: UIViewController {
     let treeInventoryCellSize = 55
     let gardenGridSpacing: CGFloat = 7
     let treeInventorySpacing: CGFloat = 8
+    let userIdx = 2
+//    let userIdx = UserDefaults.standard.integer(forKey: "userIdx")
     
+    var date: String!
     var selectedTree: Int? = nil
     var selectedGrid: Int? = nil
+    var treeList: [Tree]!
+    var treeDict: Dictionary = [Int: Tree]()
     
-    let gridIdxArr: [Int] = [1, 3, 6, 10, 14, 18, 2, 5, 9, 13, 17, 22, 4, 8, 0, 0, 21, 26, 7, 12, 0, 0, 25, 29, 11, 16, 20, 24, 28, 31, 32]
+    let gridIdxArr: [Int] = [1, 3, 6, 10, 14, 18, 2, 5, 9, 13, 17, 22, 4, 8, 0, 0, 21, 26, 7, 12, 0, 0, 25, 29, 11, 16, 20, 24, 28, 31, 15, 19, 23, 27, 30, 32]
+    let gridInverseIdxArr: [Int] = [0, 6, 1, 12, 7, 2, 18, 13, 8, 3, 24, 19, 9, 4, 30, 25, 10, 5, 31, 26, 16, 11, 32, 27, 22, 17, 33, 28, 23, 34, 29, 35]
     
     let treeImageArr: [String] = ["ios_tree1", "ios_tree2", "ios_tree3", "ios_tree4", "ios_tree5", "ios_tree6", "ios_tree7", "ios_tree8", "ios_tree9", "ios_tree10", "ios_tree11", "ios_tree11", "ios_tree12", "ios_tree13", "ios_tree14", "ios_tree15", "ios_tree16"]
     
@@ -33,6 +39,7 @@ class MainGridVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        getGardenData()
         setWaterview()
         setInventoryBackgroundView()
         
@@ -45,6 +52,48 @@ class MainGridVC: UIViewController {
         treeInventoryCV.allowsMultipleSelection = false
         gardenGridCV.allowsMultipleSelection = false
     }
+    
+    func getGardenData() {
+        GardenService.shared.getGarden(userIdx: userIdx, date: date) {
+            [weak self]
+            data in
+            
+            guard let `self` = self else { return }
+            
+            switch data {
+            case .success(let res):
+                self.treeList = res as! [Tree]
+                self.treeDict = Dictionary(uniqueKeysWithValues: self.treeList.map { ($0.location, $0) })
+                
+                print(self.treeDict)
+
+                self.gardenGridCV.reloadData()
+                
+                break
+            case .requestErr(let err):
+                print(".requestErr(\(err))")
+                break
+            case .pathErr:
+                print("경로 에러")
+                break
+            case .serverErr:
+                print("서버 에러")
+                break
+            case .networkFail:
+                self.simpleAlert(title: "통신 실패", message: "네트워크 상태를 확인하세요.")
+                break
+            }
+        }
+    }
+    
+    func setTree() {
+        for tree in treeList {
+            let indexPath = IndexPath(row: gridInverseIdxArr[tree.location], section: 0)
+            let cell = gardenGridCV.dequeueReusableCell(withReuseIdentifier: "TreeCVC", for: indexPath) as! TreeCVC
+            
+            cell.treeImageView.image = UIImage(named: "ios_tree\(tree.treeIdx)")
+        }
+    }
 
     func registerCVC() {
         let nibName = UINib(nibName: "TreeCVC", bundle: nil)
@@ -53,7 +102,7 @@ class MainGridVC: UIViewController {
     }
     
     @IBAction func backBtnAction(_ sender: Any) {
-        self.navigationController?.popViewController(animated: true)
+        self.pop()
     }
     
     func setWaterview() {
@@ -63,6 +112,49 @@ class MainGridVC: UIViewController {
     func setInventoryBackgroundView() {
         inventoryBackgroundView.setBorder(borderColor: UIColor.whiteForMainBorder, borderWidth: 0.3)
         inventoryBackgroundView.dropShadow(color: UIColor(red: 0.0 / 255.0, green: 0.0 / 255.0, blue: 0.0 / 255.0, alpha: 0.4), offSet: CGSize(width: 0, height: -1), opacity: 0.4, radius: 3)
+    }
+    
+    @IBAction func addBtnAction(_ sender: Any) {
+        if selectedTree == nil {
+            self.simpleAlert(title: "Oops!", message: "나무를 선택해주세요")
+            return
+        } else if selectedGrid == nil {
+            self.simpleAlert(title: "Oops!", message: "위치를 선택해주세요")
+            return
+        }
+        
+        let location = gridIdxArr[selectedGrid!]
+        
+        GardenService.shared.addTree(userIdx: userIdx, location: location, treeIdx: selectedTree!) {
+            data in
+            
+            switch data {
+            case .success(let message):
+                let messageStr = String(describing: message)
+                if messageStr == "일기를 써야 심을 수 있어요!" {
+                    self.simpleAlert(title: "Oops!", message: messageStr)
+                    self.pop()
+                } else if messageStr == "이미 심으셨습니다!" {
+                    self.simpleAlert(title: "Oops!", message: messageStr)
+                    self.pop()
+                } else {
+                    // Todo
+                    // 일기 성공했을 때 리로드되어야됨..
+                }
+                break
+            case .requestErr:
+                break
+            case .pathErr:
+                print("경로 에러")
+                break
+            case .serverErr:
+                print("서버 에러")
+                break
+            case .networkFail:
+                self.simpleAlert(title: "통신 실패", message: "네트워크 상태를 확인하세요.")
+                break
+            }
+        }
     }
 }
 
@@ -84,14 +176,19 @@ extension MainGridVC: UICollectionViewDataSource {
         if collectionView == self.gardenGridCV {
             cell.makeRounded(cornerRadius: 4)
             cell.setBorder(borderColor: UIColor.lightGreen, borderWidth: 2.2)
-            
-            if selectedGrid == indexPath.row {
+
+            if treeDict.keys.contains(gridIdxArr[indexPath.row]) {
+                let treeIdx: Int = treeDict[gridIdxArr[indexPath.row]]!.treeIdx
+                cell.treeImageView.image = UIImage(named: treeIdx == 16 ? "ios_weeds" : "ios_tree\(treeIdx + 1)")
+                cell.backgroundColor = UIColor.white
+            } else if selectedGrid == indexPath.row {
                 cell.backgroundColor = UIColor.lightGreenForGrid
                 cell.treeImageView.image = UIImage(named: treeImageArr[selectedTree!])
             } else {
                 cell.backgroundColor = UIColor.white
                 cell.treeImageView.image = nil
             }
+            
         } else {
             cell.treeImageView.image = UIImage(named: treeImageArr[indexPath.row])
             cell.backgroundColor = UIColor.whiteForBorder
@@ -113,15 +210,21 @@ extension MainGridVC: UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         if collectionView == self.gardenGridCV {
             if selectedTree != nil {
+                if treeDict.keys.contains(gridIdxArr[indexPath.row]) {
+                    print("...???")
+                    return }
                 selectedGrid = indexPath.row
+                
+                collectionView.reloadData()
             } else {
-                self.simpleAlert(title: "먼저!", message: "하단의 나무를 선택해주세요")
+                self.simpleAlert(title: "잠깐!", message: "나무를 먼저 선택해주세요")
             }
         } else {
             self.selectedTree = indexPath.row
+            self.selectedGrid = nil
+            
+            collectionView.reloadData()
         }
-        
-        collectionView.reloadData()
     }
     
     func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
