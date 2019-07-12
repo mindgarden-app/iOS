deb6be25cb0a884319a189288f88d4f5dfe6974b
Sunghee <630sunghee@gmail.com>
2019-07-12 18:29:28 +0900

    Create grid codable

---
+++ MindGarden/Sources/Models/Grid.swift
@@ -0,0 +1,11 @@
+//
+//  Grid.swift
+//  MindGarden
+//
+//  Created by Sunghee Lee on 12/07/2019.
+//  Copyright © 2019 Sunghee Lee. All rights reserved.
+//
+
+struct Grid: Codable {
+    let balloon: Int
+}
